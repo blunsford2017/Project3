e50802a3b367e4324c5caa3017d354efd71cc05e
B.Lunsford <breanna.sirbin@gmail.com>
2020-12-08 10:29:58 -0700

    errors in database saving fixed

--- models/user.js
+++ models/user.js
@@ -26,16 +26,19 @@ userSchema.set('toJSON', {
 });
 
 userSchema.pre('save', function(next) {
-  // this will be set to the current document
   const user = this;
   if (!user.isModified('password')) return next();
   // password has been changed - salt and hash it
-  bcrypt.hash(user.password.SALT_ROUNDS, function(err, hash) {
-    if (err) return next (err);
+  bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash) {
+    if (err) return next(err);
     // replace the user provided password with the hash
     user.password = hash;
     next();
-  })
-})
+  });
+});
+
+userSchema.methods.comparePassword = function(tryPassword, cb) {
+  bcrypt.compare(tryPassword, this.password, cb);
+};
 
 module.exports = mongoose.model('User', userSchema);
