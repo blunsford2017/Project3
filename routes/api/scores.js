c3dd7588ed959e6b3199e2e69a4f807a675f1463
B.Lunsford <breanna.sirbin@gmail.com>
2020-12-08 10:57:37 -0700

    middleware for auth added

--- routes/api/scores.js
+++ routes/api/scores.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const scoresCtrl = require('../../controllers/scores');
 
 router.get('/', scoresCtrl.highScores);
+
+/*---------- Protected Routes ----------*/
+router.use(require('../../config/auth'));
+router.post('/', scoresCtrl.create);
+
 router.post('/', scoresCtrl.create);
 
 module.exports = router;
